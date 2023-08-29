29c171e8d7e4d1713e26c4d8ccf196236e29aa6f
osyyyS <enlor.osibusek@gmail.com>
2023-08-29 21:49:29 +0200

    Add PasswordRules

---
+++ ZOOMValidation/Rules/Password/PasswordRule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZOOMValidation.Rules.Password
+{
+    public abstract class PasswordRule<T> : IValidationRule<T>
+    {
+        private readonly int minAmount, maxAmount;
+        private readonly Func<char[], int> counter;
+        public PasswordRule(Func<char[], int> counter, string errorMessage, int minAmount = 1, int maxAmount = int.MaxValue)
+        {
+            ErrorMessage = errorMessage;
+            this.counter = counter;
+            this.minAmount = minAmount;
+            this.maxAmount = maxAmount;
+        }
+
+        public string ErrorMessage { get; }
+
+        public bool Check(T value)
+        {
+            if (value is string password)
+            {
+                int count = counter(password.ToCharArray());
+                return maxAmount >= count && count >= minAmount;
+            }
+            throw new ArgumentException($"Expected string, got {typeof(T).FullName}.");
+        }
+    }
+}
---
+++ ZOOMValidation/Rules/Password/PasswordRules.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZOOMValidation.Rules.Password
+{
+    public class PasswordLengthRule<T> : PasswordRule<T>
+    {
+        public PasswordLengthRule(string errorMessage, int minimumNumbers = 1, int maximumNumbers = int.MaxValue)
+            : base(charArray => charArray.Length, errorMessage, minimumNumbers, maximumNumbers) { }
+    }
+
+    public class PasswordNumberRule<T> : PasswordRule<T>
+    {
+        public PasswordNumberRule(string errorMessage, int minimumNumbers = 1, int maximumNumbers = int.MaxValue)
+            : base(charArray => charArray.Count(c => char.IsNumber(c)), errorMessage, minimumNumbers, maximumNumbers) { }
+    }
+
+    public class PasswordUppercaseRule<T> : PasswordRule<T>
+    {
+        public PasswordUppercaseRule(string errorMessage, int minimumNumbers = 1, int maximumNumbers = int.MaxValue)
+            : base(charArray => charArray.Count(c => char.IsUpper(c)), errorMessage, minimumNumbers, maximumNumbers) { }
+    }
+
+    public class PasswordLowercaseRule<T> : PasswordRule<T>
+    {
+        public PasswordLowercaseRule(string errorMessage, int minimumNumbers = 1, int maximumNumbers = int.MaxValue)
+            : base(charArray => charArray.Count(c => char.IsLower(c)), errorMessage, minimumNumbers, maximumNumbers) { }
+    }
+
+    public class PasswordSpecialCharacterRule<T> : PasswordRule<T>
+    {
+        public PasswordSpecialCharacterRule(string errorMessage, int minimumNumbers = 1, int maximumNumbers = int.MaxValue)
+            : base(charArray => charArray.Count(c => !char.IsLetter(c) && !char.IsDigit(c)), errorMessage, minimumNumbers, maximumNumbers) { }
+    }
+}
